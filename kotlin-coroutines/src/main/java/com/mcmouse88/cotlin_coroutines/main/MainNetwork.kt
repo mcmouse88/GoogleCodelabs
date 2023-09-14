2c99a840d4d5c2217feff42f7c7adc43e8b303e8
razdobudko.s <razdobudko.s@wb.ru>
2023-09-14 22:17:24 +0200

    Init coroutines lesson

---
+++ kotlin-coroutines/src/main/java/com/mcmouse88/cotlin_coroutines/main/MainNetwork.kt
@@ -0,0 +1,33 @@
+package com.mcmouse88.cotlin_coroutines.main
+
+import com.mcmouse88.cotlin_coroutines.utils.SkipNetworkInterceptor
+import okhttp3.OkHttpClient
+import retrofit2.Call
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import retrofit2.create
+import retrofit2.http.GET
+
+private val service: MainNetwork by lazy {
+    val okHttpClient = OkHttpClient.Builder()
+        .addInterceptor(SkipNetworkInterceptor())
+        .build()
+
+    val retrofit = Retrofit.Builder()
+        .baseUrl("https://localhost/")
+        .client(okHttpClient)
+        .addConverterFactory(GsonConverterFactory.create())
+        .build()
+
+    retrofit.create()
+}
+
+fun getNetworkService() = service
+
+/**
+ * Main network interface which will fetch a new welcome title for us
+ */
+interface MainNetwork {
+    @GET("next_title.json")
+    fun fetchNextTitle(): Call<String>
+}
---
+++ kotlin-coroutines/src/main/java/com/mcmouse88/cotlin_coroutines/utils/Executors.kt
@@ -0,0 +1,8 @@
+package com.mcmouse88.cotlin_coroutines.utils
+
+import java.util.concurrent.Executors
+
+/**
+* An executor service that can run [Runnable]s off the main thread.
+*/
+val BACKGROUND = Executors.newFixedThreadPool(2)
