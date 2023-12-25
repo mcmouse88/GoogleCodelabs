6c674b4555655fe251eb5ad734755846330062e7
razdobudko.s <razdobudko.s@wb.ru>
2023-12-25 01:38:55 +0100

    More XML code migration

--- jetpack-compose-migration/src/main/java/com/mcmouse88/jetpack_compose_migration/plantdetail/PlantDetailDescription.kt
+++ jetpack-compose-migration/src/main/java/com/mcmouse88/jetpack_compose_migration/plantdetail/PlantDetailDescription.kt
@@ -1,9 +1,11 @@
 package com.mcmouse88.jetpack_compose_migration.plantdetail
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -11,6 +13,9 @@ import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.pluralStringResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import com.mcmouse88.jetpack_compose_migration.R
 import com.mcmouse88.jetpack_compose_migration.data.Plant
@@ -30,7 +35,14 @@ fun PlantDetailDescription(
 
 @Composable
 fun PlantDetailContent(plant: Plant) {
-    PlantName(name = plant.name)
+    Surface {
+        Column(
+            modifier = Modifier.padding(dimensionResource(id = R.dimen.margin_normal))
+        ) {
+            PlantName(name = plant.name)
+            PlantWatering(wateringInterval = plant.wateringInterval)
+        }
+    }
 }
 
 @Preview
@@ -67,5 +79,45 @@ private fun PlantName(name: String) {
 private fun PlantNamePreview() {
     MaterialTheme {
         PlantName(name = "Apple")
+    }
+}
+
+@Composable
+private fun PlantWatering(wateringInterval: Int) {
+    Column(
+        modifier = Modifier.fillMaxWidth()
+    ) {
+        // Same modifier used by both Texts
+        val centeredWithPaddingModifier = Modifier
+            .padding(horizontal = dimensionResource(id = R.dimen.margin_small))
+            .align(Alignment.CenterHorizontally)
+
+        val normalPadding = dimensionResource(id = R.dimen.margin_normal)
+        
+        Text(
+            text = stringResource(id = R.string.watering_needs_prefix),
+            color = MaterialTheme.colors.primaryVariant,
+            fontWeight = FontWeight.Bold,
+            modifier = centeredWithPaddingModifier.padding(top = normalPadding)
+        )
+
+        val wateringIntervalText = pluralStringResource(
+            R.plurals.watering_needs_suffix,
+            wateringInterval,
+            wateringInterval
+        )
+
+        Text(
+            text = wateringIntervalText,
+            modifier = centeredWithPaddingModifier.padding(bottom = normalPadding)
+        )
+    }
+}
+
+@Preview
+@Composable
+private fun PlantWateringPreview() {
+    MaterialTheme {
+        PlantWatering(wateringInterval = 7)
     }
 }
