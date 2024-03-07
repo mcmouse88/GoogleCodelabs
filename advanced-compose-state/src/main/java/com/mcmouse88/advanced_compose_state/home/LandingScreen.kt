2ddf5193331fe013aadce60377d788a7d0494308
razdobudko.s <razdobudko.s@wb.ru>
2024-03-07 18:23:56 +0100

    5. LaunchedEffect and rememberUpdatedState

--- advanced-compose-state/src/main/java/com/mcmouse88/advanced_compose_state/home/LandingScreen.kt
+++ advanced-compose-state/src/main/java/com/mcmouse88/advanced_compose_state/home/LandingScreen.kt
@@ -4,10 +4,16 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.painterResource
 import com.mcmouse88.advanced_compose_state.R
+import kotlinx.coroutines.delay
+
+private const val SplashWaitTime: Long = 2_000L
 
 @Composable
 fun LandingScreen(
@@ -15,11 +21,20 @@ fun LandingScreen(
     modifier: Modifier = Modifier
 ) {
     Box(
-        modifier = Modifier.fillMaxSize(),
+        modifier = modifier.fillMaxSize(),
         contentAlignment = Alignment.Center
     ) {
-        // TODO: Codelab LaunchEffect and rememberUpdateState() step
-        // TODO: Make LandingScreen disappear after loading data
+        // This will always refer to the latest onTimeout function that
+        // LandingScreen was recomposed with
+        val currentOnTimeOut by rememberUpdatedState(newValue = onTimeout)
+
+        // Create an effect that matches the lifecycle of LandingScreen.
+        // If LandingScreen recomposes or onTimeout changes,
+        // the delay shouldn't start again.
+        LaunchedEffect(key1 = Unit) {
+            delay(SplashWaitTime)
+            currentOnTimeOut.invoke()
+        }
         Image(
             painter = painterResource(id = R.drawable.ic_crane_drawer),
             contentDescription = null
--- advanced-compose-state/src/main/java/com/mcmouse88/advanced_compose_state/home/MainActivity.kt
+++ advanced-compose-state/src/main/java/com/mcmouse88/advanced_compose_state/home/MainActivity.kt
@@ -6,6 +6,10 @@ import androidx.activity.compose.setContent
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.core.view.WindowCompat
 import com.mcmouse88.advanced_compose_state.details.launchDetailsActivity
 import com.mcmouse88.advanced_compose_state.ui.theme.AdvancedStateComposeTheme
@@ -41,6 +45,11 @@ private fun MainScreen(
     Surface(
         color = MaterialTheme.colors.primary
     ) {
-        CraneHome(onExploreItemClicked = onExploreItemClicked)
+        var showLandingScreen by remember { mutableStateOf(true) }
+        if (showLandingScreen) {
+            LandingScreen(onTimeout = { showLandingScreen = false })
+        } else {
+            CraneHome(onExploreItemClicked = onExploreItemClicked)
+        }
     }
 }
