f3e6355ba65daae602b0ca2a401e58ac156666c8
razdobudko.s <razdobudko.s@wb.ru>
2024-03-08 01:24:27 +0100

    8. DisposableEffect

--- advanced-compose-state/src/main/java/com/mcmouse88/advanced_compose_state/details/MapViewUtils.kt
+++ advanced-compose-state/src/main/java/com/mcmouse88/advanced_compose_state/details/MapViewUtils.kt
@@ -3,8 +3,12 @@ package com.mcmouse88.advanced_compose_state.details
 import android.os.Bundle
 import androidx.annotation.FloatRange
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
 import androidx.compose.runtime.remember
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
 import com.google.android.libraries.maps.GoogleMap
 import com.google.android.libraries.maps.MapView
 import com.mcmouse88.advanced_compose_state.R
@@ -15,13 +19,22 @@ import com.mcmouse88.advanced_compose_state.R
 @Composable
 fun rememberMapWithLifecycle(): MapView {
     val context = LocalContext.current
-    // TODO: Codelab DisposableEffect step. Make MapView follow the lifecycle
-    return remember {
+    val mapView = remember {
         MapView(context).apply {
             id = R.id.map
-            onCreate(Bundle())
         }
     }
+
+    val lifecycle = LocalLifecycleOwner.current.lifecycle
+    DisposableEffect(key1 = lifecycle, key2 = mapView) {
+        // Make MapView follow the current lifecycle
+        val lifecycleObserver = getMapLifecycleObserver(mapView)
+        onDispose {
+            lifecycle.removeObserver(lifecycleObserver)
+        }
+    }
+
+    return mapView
 }
 
 fun GoogleMap.setZoom(
@@ -30,4 +43,18 @@ fun GoogleMap.setZoom(
     resetMinMaxZoomPreference()
     setMinZoomPreference(zoom)
     setMaxZoomPreference(zoom)
+}
+
+private fun getMapLifecycleObserver(mapView: MapView): LifecycleEventObserver {
+    return LifecycleEventObserver { _, event ->
+        when (event) {
+            Lifecycle.Event.ON_CREATE -> mapView.onCreate(Bundle())
+            Lifecycle.Event.ON_START -> mapView.onStart()
+            Lifecycle.Event.ON_RESUME -> mapView.onResume()
+            Lifecycle.Event.ON_PAUSE -> mapView.onPause()
+            Lifecycle.Event.ON_STOP -> mapView.onStop()
+            Lifecycle.Event.ON_DESTROY -> mapView.onDestroy()
+            else -> throw IllegalStateException()
+        }
+    }
 }
