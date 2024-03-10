d8711b12887dd45a6057d8684546614a78651319
razdobudko.s <razdobudko.s@wb.ru>
2024-03-10 12:48:27 +0100

    7. Request and cache PagingData in the ViewModel

--- paging-basic/src/main/java/com/mcmouse88/paging_basic/ui/ArticleViewModel.kt
+++ paging-basic/src/main/java/com/mcmouse88/paging_basic/ui/ArticleViewModel.kt
@@ -2,11 +2,15 @@ package com.mcmouse88.paging_basic.ui
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
 import com.mcmouse88.paging_basic.data.Article
 import com.mcmouse88.paging_basic.data.ArticleRepository
-import kotlinx.coroutines.flow.SharingStarted
-import kotlinx.coroutines.flow.StateFlow
-import kotlinx.coroutines.flow.stateIn
+import kotlinx.coroutines.flow.Flow
+
+private const val ITEMS_PER_PAGE = 50
 
 /**
  * ViewModel for the [ArticleActivity] screen.
@@ -19,10 +23,8 @@ class ArticleViewModel(
     /**
      * Stream of [Article]s for the UI.
      */
-    val items: StateFlow<List<Article>> = repository.articleStream
-        .stateIn(
-            scope = viewModelScope,
-            started = SharingStarted.WhileSubscribed(),
-            initialValue = emptyList()
-        )
+    val items: Flow<PagingData<Article>> = Pager(
+        config = PagingConfig(pageSize = ITEMS_PER_PAGE, enablePlaceholders = false),
+        pagingSourceFactory = { repository.articlePagingSource() }
+    ).flow.cachedIn(viewModelScope)
 }
